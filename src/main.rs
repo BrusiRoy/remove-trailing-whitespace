56a09bc5e92ceeb90bce9f5d72f059454b7e6195
Bruno Roy <brusi_roy@hotmail.com>
2017-07-22 20:32:50 -0400

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,7 @@
+
+fn main() {
+    println!("Hello, world!");
+    // 1. Try opening the file. (if it's not a valid file, print error message)
+    // 2. Read the file, if the char is '\n' and the current lenght is not 0, clear it
+    // 3. For the moment use an output file... (TODO: Do in-place...)
+}
