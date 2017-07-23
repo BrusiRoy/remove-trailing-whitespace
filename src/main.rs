fa416e3add5a39650741185f1d331c001fd28863
Bruno Roy <brusi_roy@hotmail.com>
2017-07-22 20:48:50 -0400

    Read command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,19 @@
+use std::env;
 
 fn main() {
-    println!("Hello, world!");
-    // 1. Try opening the file. (if it's not a valid file, print error message)
-    // 2. Read the file, if the char is '\n' and the current lenght is not 0, clear it
-    // 3. For the moment use an output file... (TODO: Do in-place...)
+  // 0. Read the command line arguments
+  let args: Vec<String> = env::args().collect();
+  if args.len() != 3 {
+    panic!("Invalid number of arguments");
+
+  }
+
+  let input_file  = &args[1];
+  let output_file = &args[2];
+  println!("Input file: {}", input_file);
+  println!("Output file: {}", output_file);
+
+  // 1. Try opening the file. (if it's not a valid file, print error message)
+  // 2. Read the file, if the char is '\n' and the current lenght is not 0, clear it
+  // 3. For the moment use an output file... (TODO: Do in-place...)
 }
