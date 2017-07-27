e15ba0559010aa7600710ec98a2f6c4ff8727bee
Bruno Roy <brusi_roy@hotmail.com>
2017-07-26 22:36:23 -0400

    rustfmt

--- src/main.rs
+++ src/main.rs
@@ -30,75 +30,73 @@ Options:
 ");
 
 fn main() {
-  // Read the command line arguments
-  let args: Args = Args::docopt().deserialize().unwrap_or_else(|e| e.exit());
-  println!("{:?}", args);
+    // Read the command line arguments
+    let args: Args = Args::docopt().deserialize().unwrap_or_else(|e| e.exit());
+    println!("{:?}", args);
 
-  if args.flag_in_place {
-    for file in args.arg_file {
-      let input_content = extract_string_content(&file[..]);
-      let output_content = remove_trailing_whitespace(&input_content);
-      std::fs::remove_file(&file)
-        .expect("Unable to apply in-place.");
-      write_to_file(&file[..], &output_content[..]);
+    if args.flag_in_place {
+        for file in args.arg_file {
+            let input_content = extract_string_content(&file[..]);
+            let output_content = remove_trailing_whitespace(&input_content);
+            std::fs::remove_file(&file).expect("Unable to apply in-place.");
+            write_to_file(&file[..], &output_content[..]);
+        }
+    } else if args.flag_directory {
+        // TODO: Find all the files within a directory
+    } else {
+        let input_content = extract_string_content(&args.arg_input[..]);
+        let output_content = remove_trailing_whitespace(&input_content);
+        write_to_file(&args.arg_output[..], &output_content[..]);
     }
-  } else if args.flag_directory {
-    // TODO: Find all the files within a directory
-  } else {
-    let input_content = extract_string_content(&args.arg_input[..]);
-    let output_content = remove_trailing_whitespace(&input_content);
-    write_to_file(&args.arg_output[..], &output_content[..]);
-  }
 }
 
 /// Returns a String representing the content of input file `file_name`
 fn extract_string_content(file_name: &str) -> String {
-  println!("Opening input file '{}'.", file_name);
-  let mut input_file = File::open(file_name)
-    .expect("File not found.");
+    println!("Opening input file '{}'.", file_name);
+    let mut input_file = File::open(file_name).expect("File not found.");
 
-  let mut content = String::new();
-  input_file.read_to_string(&mut content)
-    .expect("Something went wrong reading the file.");
+    let mut content = String::new();
+    input_file
+        .read_to_string(&mut content)
+        .expect("Something went wrong reading the file.");
 
-  content
+    content
 }
 
 /// Writes `content` to `file_name`
 fn write_to_file(file_name: &str, content: &str) {
-  println!("Writing result to file '{}'.", file_name);
-  let mut output_file = File::create(file_name)
-    .expect("Unable to create output file.");
-  output_file.write_all(content.as_bytes())
-    .expect("Something went wrong while writing output file.");
+    println!("Writing result to file '{}'.", file_name);
+    let mut output_file = File::create(file_name).expect("Unable to create output file.");
+    output_file
+        .write_all(content.as_bytes())
+        .expect("Something went wrong while writing output file.");
 }
 
 /// Returns a String with the same content of `str` but with no trailing whitespace
 fn remove_trailing_whitespace(str: &String) -> String {
-  let mut return_str = String::new();
-  let mut last_char_index = 0;
-  let mut start_line_index = 0;
+    let mut return_str = String::new();
+    let mut last_char_index = 0;
+    let mut start_line_index = 0;
 
-  for (i, c) in str.chars().enumerate() {
-    match c {
-      '\n' => {
-        if last_char_index != start_line_index {
-          return_str.push_str(&str[start_line_index..last_char_index + 1]);
+    for (i, c) in str.chars().enumerate() {
+        match c {
+            '\n' => {
+                if last_char_index != start_line_index {
+                    return_str.push_str(&str[start_line_index..last_char_index + 1]);
+                }
+                return_str.push('\n');
+
+                last_char_index = i + 1;
+                start_line_index = i + 1;
+            }
+            ' ' => {
+                continue;
+            }
+            _ => {
+                last_char_index = i;
+            }
         }
-        return_str.push('\n');
-
-        last_char_index  = i + 1;
-        start_line_index = i + 1;
-      }
-      ' ' => {
-        continue;
-      }
-      _ => {
-        last_char_index = i;
-      }
     }
-  }
 
-  return_str
+    return_str
 }
-
